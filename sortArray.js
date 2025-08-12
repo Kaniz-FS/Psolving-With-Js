9f9e60e8a569356c4349d34d43bb968fb403d57e
Kaniz Fatema <ravenclaw.k12@gmail.com>
2025-08-12 11:36:47 +0600

    Update sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -2,7 +2,12 @@ const num = [2, 67, 30, 51, 89, 126, 23, 78];
 num.sort();
 console.log(num);
 
-// When sorting an array of numbers in JavaScript without a compare function, it behaves like sorting strings — it compares values character by character from left to right. That’s why, for example - 126 will come before 2, because the first digit 1 is smaller than 2. To sort numbers correctly in ascending or descending order, we need to use a compare function in sort().
+// When sorting an array of numbers in JavaScript without a compare function, 
+// it behaves like sorting strings — it compares values character by character 
+// from left to right. That’s why, for example - 126 will come before 2, because 
+// the first digit 1 is smaller than 2. To sort numbers correctly in ascending or
+// descending order, we need to use a compare function in sort().
+
 
 //for ascending sort
 
